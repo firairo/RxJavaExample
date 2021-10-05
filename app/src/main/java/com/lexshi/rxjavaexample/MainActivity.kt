f07c4a1f24c1097f8b4ae8a000084ef9c5cf7e42
aliaksei_shynkarou <aliaksei_shynkarou@senla.eu>
2021-10-05 17:37:26 +0300

    add button.setOnClickListener and Observer example

--- app/src/main/java/com/lexshi/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/lexshi/rxjavaexample/MainActivity.kt
@@ -9,7 +9,8 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.BackpressureStrategy
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         val buttonObservable = findViewById<Button>(R.id.button_observable)
         val buttonFlowable = findViewById<Button>(R.id.button_flowable)
+        val buttonObservable2 = findViewById<Button>(R.id.button_observable2)
 
     //first example
         val observable = Observable.just(1,2,3)
@@ -27,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         val dispose = observable.subscribe {
             Log.e("TAG", "new data $it")
         }
+
+
+
+        //Start the stream when the button is clicked//
+        buttonObservable2.setOnClickListener { startRStream() }
 
     //observable example
         val dataObservable = dataSourceObserver()
@@ -40,8 +47,6 @@ class MainActivity : AppCompatActivity() {
             }, {
 
             })
-
-
 
     //flowable example
         val dataFlowable = dataSourceFlowable()
@@ -63,7 +68,43 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun startRStream() {
+        val myObservable = getObservable() //Create an Observable//
+        val myObserver = getObserver() //Create an Observer//
+        myObservable //Subscribe myObserver to myObservable//
+            .subscribe(myObserver)
+    }
 
+
+    //Give myObservable some data to emit//
+    private fun getObservable(): Observable<String> {
+        return Observable.just("1", "2", "3", "4", "5")
+        }
+    }
+
+    private fun getObserver(): Observer<String> {
+        return object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {}
+
+            //Every time onNext is called, print the value to Android Studio’s Logcat//
+            override fun onNext(s: String) {
+                Log.d("Main Activity", "onNext: $s")
+            }
+
+            //Called if an exception is thrown//
+            override fun onError(e: Throwable) {
+                Log.e("Main Activity", "onError: " + e.message)
+            }
+
+            //When onComplete is called, print the following to Logcat//
+            override fun onComplete() {
+                Log.d("Main Activity", "onComplete")
+            }
+        }
+    }
+
+
+    //observable example (return to onCreate)
     private fun dataSourceObserver(): Observable<Int> {
         return Observable.create { subscriber ->
             for (i in 0..100){
@@ -72,13 +113,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    //flowable example (return to onCreate)
+
     // BackpressureStrategy - Стратегия если много данных. Есть MISSING, ERROR, BUFFER, DROP, LATEST.
     // MISSING - Signals a MissingBackpressureException in case the downstream can't keep up.
     // ERROR - Выкидываем ошибку
     // BUFFER - кэшируем и постепенно выдаем
     // DROP - скипаем часть данных
     // LATEST - выводим последние
-
     private fun dataSourceFlowable(): Flowable<Int> {
         return Flowable.create ({ subscriber ->
             for (i in 0..1000000){
@@ -86,6 +129,5 @@ class MainActivity : AppCompatActivity() {
             }
 
             subscriber.onComplete()
-        }, BackpressureStrategy.BUFFER)
+        }, BackpressureStrategy.LATEST)
     }
-}
